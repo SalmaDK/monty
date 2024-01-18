d30fcb6388c728860cce94077f2b1149bb936be2
yuri-deutsh <66663102+yuri-deutsh@users.noreply.github.com>
2024-01-18 22:57:12 +0100

    Update stack_op.c

--- stack_op.c
+++ stack_op.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- *  * mul_nodes - Adds the top two elements of the stack.
+ *  * mul_nodes -- Adds the top two elements of the stack.
  *   * @stack: Pointer to a pointer pointing to top node of the stack.
  *    * @line_number: Interger representing the line number of of the opcode.
  *     */
