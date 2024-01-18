b1a928c7fef0d5040c05e010b4c40f95c99718c0
yuri-deutsh <66663102+yuri-deutsh@users.noreply.github.com>
2024-01-18 22:56:32 +0100

    Update stack_func1.c

--- stack_func1.c
+++ stack_func1.c
@@ -2,7 +2,7 @@
 
 
 /**
- *  * add_to_stack - Adds a node to the stack.
+ *  * add_to_stack -- Adds a node to the stack.
  *   * @new_node: Pointer to the new node.
  *    * @ln: Interger representing the line number of of the opcode.
  *     */
